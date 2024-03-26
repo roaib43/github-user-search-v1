5e63b726b9322ee74a1d807d3103dd1d89465acf
roaib <roaibhyder01@gmail.cm>
2024-03-27 05:02:27 +0530

    🧪 test(unit test): added unit test for Home and Users Page
    Added unit test for Home page and Users Page

---
+++ automation-test/testCases/unitTest/userPage.spec.tsx
@@ -0,0 +1,23 @@
+import { render, getByText } from '@testing-library/react'; // Importing getByText function
+import { BrowserRouter as Router } from 'react-router-dom';
+import { User } from '../../../src/pages/User/User';
+
+
+jest.mock('../../../src/hooks/useFetch', () => ({
+  useFetch: jest.fn(() => ({
+    data: null,
+    isFetching: false,
+    error: new Error('Failed to fetch data'),
+  })),
+}));
+
+describe('User Page - Error when user not found', () => {
+  it('renders error message correctly', () => {
+    const { container } = render(
+      <Router>
+        <User />
+      </Router>
+    );
+    expect(getByText(container,'Ops, something went wrong 😢')).toBeInTheDocument(); // Asserting that error message is present
+  });
+});
